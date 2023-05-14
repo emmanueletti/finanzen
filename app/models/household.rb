819f54a668778077bdc3c737beeae0edb7382aca
Emmanuel Etti <emmanueletti@gmail.com>
2023-05-13 20:36:12 -0700

    comments household to make models intention clearer

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,5 +1,7 @@
 class Household < ApplicationRecord
   validates :name, presence: true
 
+  # Everything is owned at the household level
+  # User model is used for auth and admin controls
   has_many :users, dependent: :destroy_all
 end

4c63098f6a74bf10052da7c9bbbe3d9270766dad
Emmanuel Etti <emmanueletti@gmail.com>
2023-06-05 20:37:36 -0700

    fix: starting to get the tests to not error out

--- app/models/household.rb
+++ app/models/household.rb
@@ -3,5 +3,5 @@ class Household < ApplicationRecord
 
   # Everything is owned at the household level
   # User model is used for auth and admin controls
-  has_many :users, dependent: :destroy_all
+  has_many :users, dependent: :delete_all
 end
